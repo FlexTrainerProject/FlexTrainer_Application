ed175ec5d272ca4f0ccd9543a0937d969d92ea67
Maria Naeem <naeemmaria60@gmail.com>
2024-04-29 12:14:07 +0500

    createDietPlan form done

---
+++ Project_FLEXTrainer/Forms/SubForms/createDietPlancs.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Project_FLEXTrainer.Forms.SubForms
+{
+    partial class createDietPlancs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(createDietPlancs));
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges13 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges14 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges15 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges16 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            panel5 = new Panel();
+            panel6 = new Panel();
+            panel7 = new Panel();
+            label5 = new Label();
+            btnAddMeal = new Button();
+            panelExercise = new FlowLayoutPanel();
+            label4 = new Label();
+            label2 = new Label();
+            listAllergens = new Guna.UI2.WinForms.Guna2ComboBox();
+            ListNutritionalValue = new Guna.UI2.WinForms.Guna2ComboBox();
+            label1 = new Label();
+            btnCreate = new Button();
+            panel1.SuspendLayout();
+            panel6.SuspendLayout();
+            panel7.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(42, 101, 97);
+            panel1.Controls.Add(label1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(854, 60);
+            panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Azure;
+            panel2.Dock = DockStyle.Left;
+            panel2.Location = new Point(0, 60);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(130, 458);
+            panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.Azure;
+            panel3.Dock = DockStyle.Right;
+            panel3.Location = new Point(701, 60);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(153, 458);
+            panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.Azure;
+            panel4.Dock = DockStyle.Top;
+            panel4.Location = new Point(130, 60);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(571, 46);
+            panel4.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.Azure;
+            panel5.Dock = DockStyle.Bottom;
+            panel5.Location = new Point(130, 457);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(571, 61);
+            panel5.TabIndex = 4;
+            // 
+            // panel6
+            // 
+            panel6.BackColor = Color.FromArgb(42, 101, 97);
+            panel6.Controls.Add(btnCreate);
+            panel6.Controls.Add(label4);
+            panel6.Controls.Add(label2);
+            panel6.Controls.Add(panel7);
+            panel6.Dock = DockStyle.Fill;
+            panel6.Location = new Point(130, 106);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(571, 351);
+            panel6.TabIndex = 5;
+            // 
+            // panel7
+            // 
+            panel7.BackColor = Color.MintCream;
+            panel7.Controls.Add(ListNutritionalValue);
+            panel7.Controls.Add(listAllergens);
+            panel7.Controls.Add(panelExercise);
+            panel7.Controls.Add(btnAddMeal);
+            panel7.Controls.Add(label5);
+            panel7.Dock = DockStyle.Right;
+            panel7.Location = new Point(185, 0);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(386, 351);
+            panel7.TabIndex = 0;
+            // 
+            // label5
+            // 
+            label5.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Semibold", 14F, FontStyle.Bold);
+            label5.ForeColor = Color.Black;
+            label5.Location = new Point(215, 11);
+            label5.Name = "label5";
+            label5.Size = new Size(63, 25);
+            label5.TabIndex = 6;
+            label5.Text = "Meals";
+            // 
+            // btnAddMeal
+            // 
+            btnAddMeal.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnAddMeal.FlatAppearance.BorderSize = 0;
+            btnAddMeal.FlatStyle = FlatStyle.Flat;
+            btnAddMeal.Image = (Image)resources.GetObject("btnAddMeal.Image");
+            btnAddMeal.Location = new Point(353, 9);
+            btnAddMeal.Name = "btnAddMeal";
+            btnAddMeal.Size = new Size(27, 28);
+            btnAddMeal.TabIndex = 7;
+            btnAddMeal.UseVisualStyleBackColor = true;
+            // 
+            // panelExercise
+            // 
+            panelExercise.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
+            panelExercise.BackColor = Color.White;
+            panelExercise.BorderStyle = BorderStyle.FixedSingle;
+            panelExercise.Location = new Point(215, 43);
+            panelExercise.Name = "panelExercise";
+            panelExercise.Padding = new Padding(0, 5, 0, 0);
+            panelExercise.Size = new Size(171, 308);
+            panelExercise.TabIndex = 8;
+            // 
+            // label4
+            // 
+            label4.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Semibold", 14F, FontStyle.Bold);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(21, 187);
+            label4.Name = "label4";
+            label4.Size = new Size(158, 25);
+            label4.TabIndex = 11;
+            label4.Text = "Nutritional Value";
+            label4.Click += label4_Click;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI Semibold", 14F, FontStyle.Bold);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(21, 136);
+            label2.Name = "label2";
+            label2.Size = new Size(92, 25);
+            label2.TabIndex = 9;
+            label2.Text = "Allergens";
+            // 
+            // listAllergens
+            // 
+            listAllergens.BackColor = Color.Transparent;
+            listAllergens.CustomizableEdges = customizableEdges13;
+            listAllergens.DrawMode = DrawMode.OwnerDrawFixed;
+            listAllergens.DropDownStyle = ComboBoxStyle.DropDownList;
+            listAllergens.FocusedColor = Color.FromArgb(94, 148, 255);
+            listAllergens.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            listAllergens.Font = new Font("Segoe UI", 10F);
+            listAllergens.ForeColor = Color.FromArgb(68, 88, 112);
+            listAllergens.ItemHeight = 30;
+            listAllergens.Location = new Point(16, 125);
+            listAllergens.Name = "listAllergens";
+            listAllergens.ShadowDecoration.CustomizableEdges = customizableEdges14;
+            listAllergens.Size = new Size(180, 36);
+            listAllergens.TabIndex = 9;
+            // 
+            // ListNutritionalValue
+            // 
+            ListNutritionalValue.BackColor = Color.Transparent;
+            ListNutritionalValue.CustomizableEdges = customizableEdges15;
+            ListNutritionalValue.DrawMode = DrawMode.OwnerDrawFixed;
+            ListNutritionalValue.DropDownStyle = ComboBoxStyle.DropDownList;
+            ListNutritionalValue.FocusedColor = Color.FromArgb(94, 148, 255);
+            ListNutritionalValue.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            ListNutritionalValue.Font = new Font("Segoe UI", 10F);
+            ListNutritionalValue.ForeColor = Color.FromArgb(68, 88, 112);
+            ListNutritionalValue.ItemHeight = 30;
+            ListNutritionalValue.Location = new Point(16, 176);
+            ListNutritionalValue.Name = "ListNutritionalValue";
+            ListNutritionalValue.ShadowDecoration.CustomizableEdges = customizableEdges16;
+            ListNutritionalValue.Size = new Size(180, 36);
+            ListNutritionalValue.TabIndex = 10;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Couture", 20.2499981F, FontStyle.Bold);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(12, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(280, 27);
+            label1.TabIndex = 1;
+            label1.Text = "CREATE DIET PLAN";
+            // 
+            // btnCreate
+            // 
+            btnCreate.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            btnCreate.BackColor = Color.FromArgb(170, 52, 60);
+            btnCreate.FlatStyle = FlatStyle.Flat;
+            btnCreate.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnCreate.ForeColor = Color.White;
+            btnCreate.Location = new Point(118, 279);
+            btnCreate.Name = "btnCreate";
+            btnCreate.Size = new Size(133, 42);
+            btnCreate.TabIndex = 10;
+            btnCreate.Text = "CREATE";
+            btnCreate.UseVisualStyleBackColor = false;
+            // 
+            // createDietPlancs
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(854, 518);
+            Controls.Add(panel6);
+            Controls.Add(panel5);
+            Controls.Add(panel4);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "createDietPlancs";
+            Text = "createDietPlancs";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel6.ResumeLayout(false);
+            panel6.PerformLayout();
+            panel7.ResumeLayout(false);
+            panel7.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel4;
+        private Panel panel5;
+        private Panel panel6;
+        private Panel panel7;
+        private Label label5;
+        private Button btnAddMeal;
+        private FlowLayoutPanel panelExercise;
+        private Label label4;
+        private Label label2;
+        private Guna.UI2.WinForms.Guna2ComboBox ListNutritionalValue;
+        private Guna.UI2.WinForms.Guna2ComboBox listAllergens;
+        private Label label1;
+        private Button btnCreate;
+    }
+}
---
+++ Project_FLEXTrainer/Forms/SubForms/createDietPlancs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_FLEXTrainer.Forms.SubForms
+{
+    public partial class createDietPlancs : Form
+    {
+        public createDietPlancs()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
