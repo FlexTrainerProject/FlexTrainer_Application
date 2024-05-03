18f3e549fd29747f7a2d3c3819e01531a8cfda42
Maria Naeem <naeemmaria60@gmail.com>
2024-05-03 15:49:53 +0500

    halloo

--- Project_FLEXTrainer/obj/Debug/net8.0-windows/Project_FLEXTrainer.AssemblyInfo.cs
+++ Project_FLEXTrainer/obj/Debug/net8.0-windows/Project_FLEXTrainer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_FLEXTrainer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e8e1ded1fd57066f2424195df39f63270fc3684")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ad49b239b2cd3301739aba138a94069e180d6ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_FLEXTrainer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project_FLEXTrainer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
