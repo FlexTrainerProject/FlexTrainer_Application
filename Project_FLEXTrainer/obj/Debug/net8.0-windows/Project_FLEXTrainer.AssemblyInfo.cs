f4a06efe09289c61d1154d7707f39b47c29351c7
hamdan-sajid <hamdansajid0@gmail.com>
2024-05-09 21:57:49 +0500

    a

--- Project_FLEXTrainer/obj/Debug/net8.0-windows/Project_FLEXTrainer.AssemblyInfo.cs
+++ Project_FLEXTrainer/obj/Debug/net8.0-windows/Project_FLEXTrainer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_FLEXTrainer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38cee2e6cd800b92df67d10f4142aed53cd9cdad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_FLEXTrainer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project_FLEXTrainer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
