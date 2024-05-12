6a14f274959a505f72ac064faeccb73f1ad11ac1
mariathv <naeemmaria60@gmail.com>
2024-05-12 21:17:43 +0500

    finalgg

--- Project_FLEXTrainer/Trainer/Forms/Feedbacks.Designer.cs
+++ Project_FLEXTrainer/Trainer/Forms/Feedbacks.Designer.cs
@@ -28,24 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Feedbacks));
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             navPanel = new Panel();
+            panelTemplate = new Panel();
+            panel3 = new Panel();
+            nameLabel = new Label();
+            experienceLabel = new Label();
+            genderLabel = new Label();
             panel1 = new Panel();
             label3 = new Label();
             label2 = new Label();
             label1 = new Label();
             searchBar = new Guna.UI2.WinForms.Guna2TextBox();
             panelContainer = new Panel();
-            panelTemplate = new Panel();
-            panel3 = new Panel();
-            nameLabel = new Label();
-            experienceLabel = new Label();
-            genderLabel = new Label();
             navPanel.SuspendLayout();
-            panel1.SuspendLayout();
             panelTemplate.SuspendLayout();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // navPanel
@@ -60,90 +60,6 @@
             navPanel.Name = "navPanel";
             navPanel.Size = new Size(1100, 52);
             navPanel.TabIndex = 5;
-            // 
-            // panel1
-            // 
-            panel1.BackColor = Color.FromArgb(125, 42, 40);
-            panel1.Controls.Add(label3);
-            panel1.Controls.Add(label2);
-            panel1.Dock = DockStyle.Right;
-            panel1.Location = new Point(867, 0);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(233, 52);
-            panel1.TabIndex = 13;
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.ForeColor = Color.White;
-            label3.Location = new Point(120, 9);
-            label3.Name = "label3";
-            label3.Size = new Size(61, 30);
-            label3.TabIndex = 14;
-            label3.Text = "nunu";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Dock = DockStyle.Left;
-            label2.Font = new Font("Segoe UI Semibold", 18.25F, FontStyle.Bold);
-            label2.ForeColor = Color.White;
-            label2.Location = new Point(0, 0);
-            label2.Name = "label2";
-            label2.Padding = new Padding(5, 7, 0, 0);
-            label2.Size = new Size(114, 42);
-            label2.TabIndex = 13;
-            label2.Text = "RATING:";
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Dock = DockStyle.Left;
-            label1.Font = new Font("Segoe UI Semibold", 18.25F, FontStyle.Bold);
-            label1.ForeColor = Color.White;
-            label1.Location = new Point(0, 0);
-            label1.Name = "label1";
-            label1.Padding = new Padding(5, 7, 0, 0);
-            label1.Size = new Size(221, 42);
-            label1.TabIndex = 12;
-            label1.Text = "VIEW FEEDBACKS";
-            // 
-            // searchBar
-            // 
-            searchBar.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            searchBar.BorderColor = Color.White;
-            searchBar.BorderRadius = 12;
-            searchBar.BorderThickness = 2;
-            searchBar.CustomizableEdges = customizableEdges3;
-            searchBar.DefaultText = "";
-            searchBar.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
-            searchBar.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
-            searchBar.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
-            searchBar.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
-            searchBar.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
-            searchBar.Font = new Font("Segoe UI", 9F);
-            searchBar.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
-            searchBar.IconRight = (Image)resources.GetObject("searchBar.IconRight");
-            searchBar.Location = new Point(1776, 12);
-            searchBar.Name = "searchBar";
-            searchBar.Padding = new Padding(0, 20, 0, 0);
-            searchBar.PasswordChar = '\0';
-            searchBar.PlaceholderText = "Search Member";
-            searchBar.SelectedText = "";
-            searchBar.ShadowDecoration.CustomizableEdges = customizableEdges4;
-            searchBar.Size = new Size(259, 28);
-            searchBar.TabIndex = 11;
-            // 
-            // panelContainer
-            // 
-            panelContainer.AutoScroll = true;
-            panelContainer.BackColor = Color.WhiteSmoke;
-            panelContainer.Dock = DockStyle.Fill;
-            panelContainer.Location = new Point(0, 52);
-            panelContainer.Name = "panelContainer";
-            panelContainer.Size = new Size(1100, 495);
-            panelContainer.TabIndex = 12;
             // 
             // panelTemplate
             // 
@@ -199,6 +115,90 @@
             genderLabel.TabIndex = 2;
             genderLabel.Text = "Gender";
             // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(125, 42, 40);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Dock = DockStyle.Right;
+            panel1.Location = new Point(867, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(233, 52);
+            panel1.TabIndex = 13;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(120, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(61, 30);
+            label3.TabIndex = 14;
+            label3.Text = "nunu";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Dock = DockStyle.Left;
+            label2.Font = new Font("Segoe UI Semibold", 18.25F, FontStyle.Bold);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(0, 0);
+            label2.Name = "label2";
+            label2.Padding = new Padding(5, 7, 0, 0);
+            label2.Size = new Size(114, 42);
+            label2.TabIndex = 13;
+            label2.Text = "RATING:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Dock = DockStyle.Left;
+            label1.Font = new Font("Segoe UI Semibold", 18.25F, FontStyle.Bold);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(0, 0);
+            label1.Name = "label1";
+            label1.Padding = new Padding(5, 7, 0, 0);
+            label1.Size = new Size(221, 42);
+            label1.TabIndex = 12;
+            label1.Text = "VIEW FEEDBACKS";
+            // 
+            // searchBar
+            // 
+            searchBar.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            searchBar.BorderColor = Color.White;
+            searchBar.BorderRadius = 12;
+            searchBar.BorderThickness = 2;
+            searchBar.CustomizableEdges = customizableEdges1;
+            searchBar.DefaultText = "";
+            searchBar.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            searchBar.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            searchBar.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            searchBar.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            searchBar.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            searchBar.Font = new Font("Segoe UI", 9F);
+            searchBar.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            searchBar.IconRight = (Image)resources.GetObject("searchBar.IconRight");
+            searchBar.Location = new Point(1776, 12);
+            searchBar.Name = "searchBar";
+            searchBar.Padding = new Padding(0, 20, 0, 0);
+            searchBar.PasswordChar = '\0';
+            searchBar.PlaceholderText = "Search Member";
+            searchBar.SelectedText = "";
+            searchBar.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            searchBar.Size = new Size(259, 28);
+            searchBar.TabIndex = 11;
+            // 
+            // panelContainer
+            // 
+            panelContainer.AutoScroll = true;
+            panelContainer.BackColor = Color.WhiteSmoke;
+            panelContainer.Dock = DockStyle.Fill;
+            panelContainer.Location = new Point(0, 52);
+            panelContainer.Name = "panelContainer";
+            panelContainer.Size = new Size(1100, 495);
+            panelContainer.TabIndex = 12;
+            // 
             // Feedbacks
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
@@ -211,10 +211,10 @@
             Text = "Feedbacks";
             navPanel.ResumeLayout(false);
             navPanel.PerformLayout();
-            panel1.ResumeLayout(false);
-            panel1.PerformLayout();
             panelTemplate.ResumeLayout(false);
             panelTemplate.PerformLayout();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
         }
 
